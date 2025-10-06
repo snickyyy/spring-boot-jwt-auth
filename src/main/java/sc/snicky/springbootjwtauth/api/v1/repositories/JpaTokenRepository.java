be7a9290a2361f3e6c1caafd6d7ff58bfdb63204
sc.snicky <leon12345671234@gmail.com>
2025-10-06 18:16:18 +0200

    mark all repos with annotation @Repository

--- src/main/java/sc/snicky/springbootjwtauth/api/v1/repositories/JpaTokenRepository.java
+++ src/main/java/sc/snicky/springbootjwtauth/api/v1/repositories/JpaTokenRepository.java
@@ -1,7 +1,9 @@
 package sc.snicky.springbootjwtauth.api.v1.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import sc.snicky.springbootjwtauth.api.v1.domain.models.Token;
 
+@Repository
 public interface JpaTokenRepository extends JpaRepository<Token, Integer> {
 }
