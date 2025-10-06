1d7b3ae804a36e2cec356d9333d3643ee0a4b979
sc.snicky <leon12345671234@gmail.com>
2025-10-06 18:11:34 +0200

    fix naming in JpaUserRepository

--- src/main/java/sc/snicky/springbootjwtauth/api/v1/repositories/JpaUserRepository.java
+++ src/main/java/sc/snicky/springbootjwtauth/api/v1/repositories/JpaUserRepository.java
@@ -7,8 +7,8 @@ public interface JpaUserRepository extends JpaRepository<User, Integer> {
     /**
      * Find a user by their username.
      *
-     * @param username the username to search for
+     * @param email the username to search for
      * @return the User entity if found, otherwise null
      */
-    User findByUsername(String username);
+    User findByEmail(String email);
 }
