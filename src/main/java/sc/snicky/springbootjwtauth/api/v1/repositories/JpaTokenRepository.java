73e2f787192de6dd1b2f2e96a92bbc41811a3243
sc.snicky <leon12345671234@gmail.com>
2025-10-06 19:05:01 +0200

    fix id type in JpaTokenRepository

--- src/main/java/sc/snicky/springbootjwtauth/api/v1/repositories/JpaTokenRepository.java
+++ src/main/java/sc/snicky/springbootjwtauth/api/v1/repositories/JpaTokenRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sc.snicky.springbootjwtauth.api.v1.domain.models.Token;
 
+import java.util.UUID;
+
 @Repository
-public interface JpaTokenRepository extends JpaRepository<Token, Integer> {
+public interface JpaTokenRepository extends JpaRepository<Token, UUID> {
 }
