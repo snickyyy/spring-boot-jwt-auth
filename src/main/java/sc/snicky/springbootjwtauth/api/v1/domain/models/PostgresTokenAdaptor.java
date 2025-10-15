ca60dee78cacb821b48ee36613530c83c0a338d7
sc.snicky <leon12345671234@gmail.com>
2025-10-15 15:43:49 +0200

    create refresh token representation

---
+++ src/main/java/sc/snicky/springbootjwtauth/api/v1/domain/models/PostgresTokenAdaptor.java
@@ -0,0 +1,33 @@
+package sc.snicky.springbootjwtauth.api.v1.domain.models;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@Builder
+@RequiredArgsConstructor
+public class PostgresTokenAdaptor implements RefreshTokenDetails {
+    private UUID token;
+    private Integer userId;
+    private Instant expiry;
+    private Instant createdAt;
+
+    /**
+     * Creates a new instance of {@link PostgresTokenAdaptor} from the given {@link Token}.
+     *
+     * @param token the source token object
+     * @return a new {@link PostgresTokenAdaptor} with fields populated from the token
+     */
+    public static  PostgresTokenAdaptor ofToken(Token token) {
+        return PostgresTokenAdaptor.builder()
+                .token(token.getId())
+                .userId(token.getUser().getId())
+                .createdAt(token.getCreatedAt())
+                .expiry(token.getExp())
+                .build();
+    }
+}
