ecd3b6db24d2688cefbe21123eef59e6356364cb
sc.snicky <leon12345671234@gmail.com>
2025-10-15 20:18:12 +0200

    change relation from token to user

--- src/main/java/sc/snicky/springbootjwtauth/api/v1/domain/models/Token.java
+++ src/main/java/sc/snicky/springbootjwtauth/api/v1/domain/models/Token.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -44,7 +44,7 @@ public class Token extends BaseEntity<UUID> implements Serializable {
      * Many tokens can belong to one user.
      * On user deletion, all associated tokens are also deleted (cascade).
      */
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "user_id")
     @EqualsAndHashCode.Exclude
