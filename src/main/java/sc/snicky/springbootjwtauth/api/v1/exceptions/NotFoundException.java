4cdf681e7a9fe93e1fa1aac554bef7ee7f91961f
sc.snicky <leon12345671234@gmail.com>
2025-10-15 23:58:47 +0200

    add javadoc on exception

--- src/main/java/sc/snicky/springbootjwtauth/api/v1/exceptions/NotFoundException.java
+++ src/main/java/sc/snicky/springbootjwtauth/api/v1/exceptions/NotFoundException.java
@@ -1,6 +1,11 @@
 package sc.snicky.springbootjwtauth.api.v1.exceptions;
 
 public abstract class NotFoundException extends RuntimeException {
+    /**
+     * Constructor for NotFoundException.
+     *
+     * @param message error message
+     */
     public NotFoundException(String message) {
         super(message);
     }
--- src/main/java/sc/snicky/springbootjwtauth/api/v1/exceptions/business/security/RefreshTokenNotValid.java
+++ src/main/java/sc/snicky/springbootjwtauth/api/v1/exceptions/business/security/RefreshTokenNotValid.java
@@ -3,6 +3,11 @@ package sc.snicky.springbootjwtauth.api.v1.exceptions.business.security;
 import sc.snicky.springbootjwtauth.api.v1.exceptions.ConflictException;
 
 public class RefreshTokenNotValid extends ConflictException {
+    /**
+     * Constructs a new RefreshTokenNotValid with the specified detail message.
+     *
+     * @param message the detail message
+     */
     public RefreshTokenNotValid(String message) {
         super(message);
     }
