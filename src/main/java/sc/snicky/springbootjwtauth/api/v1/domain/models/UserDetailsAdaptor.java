1258f9824a073f177762499944bccf32f48100be
sc.snicky <leon12345671234@gmail.com>
2025-10-21 22:53:54 +0200

    added javadoc to methods of UserDetailsAdaptor

--- src/main/java/sc/snicky/springbootjwtauth/api/v1/domain/models/UserDetailsAdaptor.java
+++ src/main/java/sc/snicky/springbootjwtauth/api/v1/domain/models/UserDetailsAdaptor.java
@@ -16,6 +16,13 @@ public class UserDetailsAdaptor implements UserDetails {
     private String username;
     private String password;
 
+    /**
+     * Creates a UserDetailsAdaptor from a given User entity.
+     * Maps user roles to GrantedAuthority and sets username and password.
+     *
+     * @param user the User entity to adapt
+     * @return a UserDetailsAdaptor instance
+     */
     public static UserDetailsAdaptor ofUser(User user) {
         return UserDetailsAdaptor.builder()
                 .grantedAuthorities(user.getRoles().stream()
@@ -27,16 +34,28 @@ public class UserDetailsAdaptor implements UserDetails {
                 .build();
     }
 
+    /**
+     * Returns the authorities granted to the user.
+     * @return a collection of granted authorities
+     */
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return grantedAuthorities;
     }
 
+    /**
+     * Returns the password used to authenticate the user.
+     * @return the user's password
+     */
     @Override
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Returns the username used to authenticate the user.
+     * @return the user's username
+     */
     @Override
     public String getUsername() {
         return username;
