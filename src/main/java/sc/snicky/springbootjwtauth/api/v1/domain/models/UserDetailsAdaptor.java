5fa6eef7d4f40167a9c9317806863604cbcd0d87
sc.snicky <leon12345671234@gmail.com>
2025-10-23 00:26:36 +0200

    fix CWE-374

--- src/main/java/sc/snicky/springbootjwtauth/api/v1/domain/models/UserDetailsAdaptor.java
+++ src/main/java/sc/snicky/springbootjwtauth/api/v1/domain/models/UserDetailsAdaptor.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
@@ -40,7 +41,7 @@ public class UserDetailsAdaptor implements UserDetails {
      */
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return grantedAuthorities;
+        return List.copyOf(grantedAuthorities);
     }
 
     /**
