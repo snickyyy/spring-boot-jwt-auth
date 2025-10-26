2dfc02f77ed1ce3ae4a4b1258bb79a07b5148d83
sc.snicky <leon12345671234@gmail.com>
2025-10-26 15:19:52 +0100

    fixed TokensManagerTest

--- src/test/java/unit/sc/snicky/springbootjwtauth/api/v1/services/TokensManagerTest.java
+++ src/test/java/unit/sc/snicky/springbootjwtauth/api/v1/services/TokensManagerTest.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import sc.snicky.springbootjwtauth.api.v1.domain.enums.ERole;
-import sc.snicky.springbootjwtauth.api.v1.domain.models.PostgresTokenAdaptor;
+import sc.snicky.springbootjwtauth.api.v1.domain.models.BasicRefreshToken;
+import sc.snicky.springbootjwtauth.api.v1.domain.models.JpaRefreshTokenDetailsAdaptor;
 import sc.snicky.springbootjwtauth.api.v1.domain.models.Role;
-import sc.snicky.springbootjwtauth.api.v1.domain.models.Token;
 import sc.snicky.springbootjwtauth.api.v1.domain.models.User;
 import sc.snicky.springbootjwtauth.api.v1.services.AccessTokenServiceImpl;
 import sc.snicky.springbootjwtauth.api.v1.services.RefreshTokenServiceImpl;
@@ -18,17 +20,21 @@ import sc.snicky.springbootjwtauth.api.v1.services.TokensManagerImpl;
 import java.time.Instant;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @Tag("unit")
 @ExtendWith(MockitoExtension.class)
 public class TokensManagerTest {
+    private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     private static final String TEST_EMAIL = "testuser@test.te";
     private static final String TEST_PASSWORD = "testpassword";
     private static final Long TEST_REFRESH_TOKEN_DURATION = 9000000L;
+    private static final UUID TEST_TOKEN = UUID.randomUUID();
+    private static final String TEST_HASHED_TOKEN = passwordEncoder.encode(TEST_TOKEN.toString());
 
     @Mock
     private RefreshTokenServiceImpl refreshTokenService;
@@ -44,13 +50,13 @@ public class TokensManagerTest {
     void generateTokensWithSuccess() {
         var user = buildUser();
         user.setId(1);
-        var token = PostgresTokenAdaptor.ofToken(buildToken(user));
-        when(refreshTokenService.generate(1)).thenReturn(token);
+        var token = buildToken(user);
+        when(refreshTokenService.generate(1)).thenReturn(JpaRefreshTokenDetailsAdaptor.ofToken(TEST_TOKEN, token));
 
         var result = tokensManager.generateTokens(1);
 
         assertNotNull(result);
-        assertEquals(token.getToken().toString(), result.refreshToken());
+        assertTrue(passwordEncoder.matches(result.refreshToken(), token.getToken()));
 
         verify(refreshTokenService).generate(1);
     }
@@ -59,16 +65,15 @@ public class TokensManagerTest {
     void refreshTokensWithSuccess() {
         var user = buildUser();
         user.setId(1);
-        var oldToken = buildToken(user);
-        var newToken = PostgresTokenAdaptor.ofToken(buildToken(user));
-        when(refreshTokenService.rotate(oldToken.getId())).thenReturn(newToken);
+        var newToken = buildToken(user);
+        when(refreshTokenService.rotate(TEST_TOKEN)).thenReturn(JpaRefreshTokenDetailsAdaptor.ofToken(TEST_TOKEN, newToken));
 
-        var result = tokensManager.refreshTokens(oldToken.getId().toString());
+        var result = tokensManager.refreshTokens(TEST_TOKEN.toString());
 
         assertNotNull(result);
-        assertEquals(newToken.getToken().toString(), result.refreshToken());
+        assertTrue(passwordEncoder.matches(result.refreshToken(),newToken.getToken()));
 
-        verify(refreshTokenService).rotate(oldToken.getId());
+        verify(refreshTokenService).rotate(TEST_TOKEN);
     }
 
     private User buildUser() {
@@ -80,11 +85,11 @@ public class TokensManagerTest {
         return user;
     }
 
-    private Token buildToken(User user) {
-        return Token.builder()
-                .id(UUID.randomUUID())
+    private BasicRefreshToken buildToken(User user) {
+        return BasicRefreshToken.builder()
+                .token(TEST_HASHED_TOKEN)
                 .user(user)
-                .exp(Instant.now().plusMillis(TEST_REFRESH_TOKEN_DURATION))
+                .expiresAt(Instant.now().plusMillis(TEST_REFRESH_TOKEN_DURATION))
                 .build();
     }
 }
