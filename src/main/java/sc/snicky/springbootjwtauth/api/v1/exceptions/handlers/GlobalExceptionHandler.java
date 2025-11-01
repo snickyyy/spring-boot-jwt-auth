75379053a92791481746f5eeb5afe39b7632aeef
sc.snicky <leon12345671234@gmail.com>
2025-11-01 13:13:58 +0100

    added GlobalExceptionHandler

---
+++ src/main/java/sc/snicky/springbootjwtauth/api/v1/exceptions/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package sc.snicky.springbootjwtauth.api.v1.exceptions.handlers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import sc.snicky.springbootjwtauth.api.v1.dtos.responses.ErrorResponse;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    /**
+     * Handles all uncaught exceptions in the application.
+     *
+     * @param ex the exception that was thrown
+     * @return a ResponseEntity containing an ErrorResponse with details about the error
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        log.error(ex.getMessage(), ex);
+        return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(
+                "Internal Server Error",
+                "",
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                java.time.Instant.now()
+        ));
+    }
+}
